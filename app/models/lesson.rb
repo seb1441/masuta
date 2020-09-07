0af9a6e5f04f0da0e4e21b34b7457bcacbcfc42e
seb1441 <sebastienauriault@gmail.com>
2020-09-07 17:50:08 -0400

    Fix Lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -9,15 +9,4 @@ class Lesson < ApplicationRecord
   #   where(arel_table[:title].matches("%#{query}%"))
   #     .or(where(Category.arel_table[:title].matches("%#{query}%")))
   # }
-
-  def self.enhance_column(column_name, title: nil)
-    @enhanced_columns ||= []
-    @enhanced_columns.push EnhancedColumn.new(name: column_name, title: title)
-  end
-
-  class << self
-    attr_reader :enhanced_columns
-  end
-
-  enhance_column :category_id, title: "Category"
 end
