285c49d0eda2f5f7acd136952d2c9a656aea0744
websebdev <sebastienauriault@gmail.com>
2020-12-26 21:07:25 -0500

    Add debugging console messages for chat

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -2,13 +2,17 @@ import ApplicationController from './application_controller'
 
 export default class extends ApplicationController {
   connect() {
+    console.log('chat controller connected')
     window.addEventListener('turbo:submit-end', (event) => {
+      console.log('turbo:submit-end', event)
       if (event.detail.fetchResponse.response.url.includes("/chat_rooms/10/chat_messages")) {
         var observer = new MutationObserver(function(mutations) {
           mutations.forEach(function(mutation) {
             let lastMessage = mutation.addedNodes[0].children[0]
 
             const userId = window.location.href.split('/')[5]
+            console.log('userId', userId)
+            console.log('lastMessage userId', lastMessage.dataset.userId)
             if (lastMessage.dataset.userId != userId) {
               lastMessage.classList.remove('bg-gray-300')
               lastMessage.classList.add('bg-blue-300')
