9ccf713eb0a0c48d8b8bf5225cf9672f175b70be
websebdev <sebastienauriault@gmail.com>
2020-12-30 18:12:50 -0500

    Fix alert color and paginate chat messages

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,5 +1,6 @@
 class ChatRoomsController < ApplicationController
   def show
     @chat_room = ChatRoom.find(params[:id])
+    @chat_messages = @chat_room.chat_messages.order(:created_at)
   end
 end
